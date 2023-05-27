34eb7fc216d9dd864be778cea2d7a8ec32ae06e4
Mohammed Salloot <93588232+MohammedSallout@users.noreply.github.com>
2023-05-27 19:25:21 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import path from 'path'
+//import path from 'path'
 import router from './router/index.js'
 import express from 'express'
 import cookieParser from 'cookie-parser'
@@ -11,12 +11,12 @@ app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(express.static('public'))
-app.use('/api', router)
+//app.use(express.static('public'))
+//app.use('/api', router)
 app.use('/api/v1',router);
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/public/index.html'))
+    res.sendFile('../client/public/index.html')
 })
 
 export default app
